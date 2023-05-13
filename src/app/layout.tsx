2d57bd28df7f762ce3a36598574f9d8091f6f064
Markus Antoni Suwignyo <markus.suwignyo@traveloka.com>
2023-05-14 01:08:14 +0700

    add select font style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import "./globals.css";
-import { Inconsolata, Inter, Lora } from "next/font/google";
 import Image from "next/image";
 import { SearchBar } from "@/components/SearchBar";
 import { Analytics } from "@vercel/analytics/react";
-
-const inter = Inter({ subsets: ["latin"] });
-const lora = Lora({ subsets: ["latin"] });
-const inconsolata = Inconsolata({ subsets: ["latin"] });
+import SelectFontStyle from "@/components/SelectFontStyle";
+import { ThemeProvider } from "@/stores/ThemeContext";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -14,13 +11,12 @@ interface RootLayoutProps {
 
 export default function RootLayout(props: RootLayoutProps) {
   const { children } = props;
-
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <ThemeProvider>
         <div className="p-6 md:p-10 lg:max-w-3xl lg:mx-auto">
           <div className="flex flex-col gap-6">
-            <div>
+            <div className="flex justify-between items-center">
               <Image
                 className="md:hidden"
                 src="/logo.svg"
@@ -35,13 +31,14 @@ export default function RootLayout(props: RootLayoutProps) {
                 height={36}
                 alt="test"
               />
+              <SelectFontStyle />
             </div>
             <SearchBar />
             <div>{children}</div>
           </div>
         </div>
         <Analytics />
-      </body>
+      </ThemeProvider>
     </html>
   );
 }
